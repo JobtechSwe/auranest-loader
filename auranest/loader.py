8874cc0249f35ffc4bd36d2942347d8b3751f402
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-09-24 15:21:21 +0200

    avoid recursive method calls

--- auranest/loader.py
+++ auranest/loader.py
@@ -8,23 +8,25 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def load_ad(ad_id, ts, results, index, fail_count=0):
-    try:
-        r = requests.get(settings.AURANEST_DETAILS_URL,
-                         auth=(settings.AURANEST_USER, settings.AURANEST_PASSWORD),
-                         params={'id': ad_id}, timeout=60)
-        r.raise_for_status()
-        auranest_ad = r.json()[0]
-        if auranest_ad:
-            auranest_ad['updatedAt'] = ts
-            results[index] = auranest_ad
-    except requests.exceptions.RequestException as e:
-        fail_count += 1
-        log.warn('Failed to download ad %s %d times' % (ad_id, fail_count))
-        if fail_count > 10:
-            log.error("Unable to continue loading data from Auranest", e)
-            sys.exit(1)
-        load_ad(ad_id, results, index, fail_count)
+def load_ad(ad_id, ts, results, index):
+    fail_count = 0
+    while True:
+        try:
+            r = requests.get(settings.AURANEST_DETAILS_URL,
+                             auth=(settings.AURANEST_USER, settings.AURANEST_PASSWORD),
+                             params={'id': ad_id}, timeout=60)
+            r.raise_for_status()
+            auranest_ad = r.json()[0]
+            if auranest_ad:
+                auranest_ad['updatedAt'] = ts
+                results[index] = auranest_ad
+                break
+        except requests.exceptions.RequestException as e:
+            fail_count += 1
+            log.warn('Failed to download ad "%s" %d times' % (ad_id, fail_count))
+            if fail_count > 10:
+                log.error("Unable to continue loading data from Auranest", e)
+                sys.exit(1)
 
 
 def load(next_date, next_id=None):
